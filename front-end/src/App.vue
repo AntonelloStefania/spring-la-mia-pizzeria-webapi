231ac2e9b6606f7e9fa17c1658ede903bbcde4dd
AntonelloStefania <antonelloste94@gmail.com>
2023-12-14 14:19:08 +0100

    aggiunto progetto front-end e index

---
+++ front-end/src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <pizzaIndex :pizzas="pizzas"/>
+</template>
+
+<script setup>
+import {  onMounted , ref} from 'vue';
+import axios from "axios";
+
+
+import pizzaIndex from './components/pizzaIndex.vue';
+
+    const pizzas = ref(null);
+
+
+  const updatePizzas = async () => {
+  const data = await axios.get("http://localhost:8080/api/v1.0/pizzas");
+  pizzas.value = data.data;
+};
+
+onMounted(updatePizzas);
+
+</script>
---
+++ front-end/src/components/pizzaIndex.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div>
+    <h1>Pizzas</h1>
+    <ul>
+      <li v-for="pizza in pizzas" 
+        :key="pizza.id">
+        <p>{{ pizza.name }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';  
+
+const props = defineProps({
+    pizzas: {
+        type: Array,
+        required:true
+    }
+
+});
+</script>
